55326b2bdb933e1bf16c67c0190c7d6bbba03fb3
ararem <48875125+Ararem@users.noreply.github.com>
2022-11-08 00:45:06 +1000

    A bit more logging testing

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,26 @@
+use std::io;
+
 use color_eyre::eyre::{self};
+use color_eyre::owo_colors::OwoColorize;
 use eyre::eyre;
 use pretty_assertions::{self, assert_eq, assert_ne, assert_str_eq};
 use shadow_rs::shadow;
 use tracing::*;
+use tracing_subscriber::fmt::format::FmtSpan;
+use tracing_subscriber::fmt::{format, time};
+use tracing_subscriber::util::SubscriberInitExt;
 
 shadow!(build); //Required for shadow-rs to work
 
 #[instrument]
 fn main() -> eyre::Result<()> {
+    //Init the important stuff
     init_eyre()?;
-    init_tracing("trace")?;
-    //Same for tracing
-    tracing::info_span!("test");
-    event!(Level::INFO, "somthing happened");
+    init_tracing()?;
+
+    let _span = info_span!("test_span").entered();
+    let _span = info_span!("child_span").entered();
+    event!(Level::INFO, "something happened");
     info!("TEST)");
 
     let string = "string";
@@ -30,18 +38,32 @@ fn main() -> eyre::Result<()> {
 /// [`tracing_subscriber::EnvFilter`][EnvFilter].
 ///
 /// [EnvFilter]: https://docs.rs/tracing-subscriber/latest/tracing_subscriber/struct.EnvFilter.html
-fn init_tracing(filter_directives: &str) -> eyre::Result<()> {
-    use tracing_error::ErrorLayer;
-    use tracing_subscriber::fmt::{self, format::FmtSpan};
-    use tracing_subscriber::prelude::*;
+fn init_tracing() -> eyre::Result<()> {
+    use tracing_error::*;
+    use tracing_subscriber::layer::SubscriberExt;
+    use tracing_subscriber::*;
 
-    let fmt_layer = fmt::layer()
-        .with_target(false)
-        .with_span_events(FmtSpan::ACTIVE);
+    let standard_layer = fmt::layer()
+        .event_format(format().compact())
+        .with_ansi(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_target(true)
+        .with_span_events(FmtSpan::ACTIVE)
+        .log_internal_errors(true)
+        .with_file(true)
+        .with_level(true)
+        .with_line_number(true)
+        // .with_timer(time())
+        .with_writer(io::stdout)
+        // .with_test_writer()
+        ;
+
+    let error_layer = ErrorLayer::default();
 
     tracing_subscriber::registry()
-        .with(fmt_layer)
-        .with(ErrorLayer::default())
+        .with(standard_layer)
+        .with(error_layer)
         .init();
 
     Ok(())
@@ -49,5 +71,6 @@ fn init_tracing(filter_directives: &str) -> eyre::Result<()> {
 
 fn init_eyre() -> eyre::Result<()> {
     color_eyre::install()?;
+
     Ok(())
 }
