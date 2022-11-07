1d5c9bd3b4d0ed1f648ab1f798beb1e6451f733b
ararem <48875125+Ararem@users.noreply.github.com>
2022-11-07 21:38:32 +1000

    testing with eyre

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::fmt::Error;
 use shadow_rs::{shadow, Format};
 use color_eyre::eyre::{self, WrapErr};
+use eyre::*;
 use tracing::instrument;
 
 shadow!(build); //Required for shadow-rs to work
@@ -9,6 +11,7 @@ fn main() -> eyre::Result<()> {
     color_eyre::install()?; //Set up eyre (with colours) for error handling
 
     println!("Hello, world!");
-
-    Ok(())
+    let string = "string";
+    
+    return Err(eyre!("Test lol: {} {asd} {string}", 1, asd=69420));
 }
