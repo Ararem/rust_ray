a4b6fb6b4fde5be5660f4d21c7219d965e2fe7f6
ararem <48875125+Ararem@users.noreply.github.com>
2022-11-08 15:21:14 +1000

    More fiddling and adding packages

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,33 @@
+#![warn(missing_docs)]
+
+//!
+
 use std::io;
 
 use color_eyre::eyre::{self};
-use color_eyre::owo_colors::OwoColorize;
 use eyre::eyre;
 use pretty_assertions::{self, assert_eq, assert_ne, assert_str_eq};
 use shadow_rs::shadow;
+use tracing::metadata::LevelFilter;
 use tracing::*;
-use tracing_subscriber::fmt::format::FmtSpan;
-use tracing_subscriber::fmt::{format, time};
-use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::fmt::{format::*, time};
+use tracing_subscriber::util::TryInitError;
 
 shadow!(build); //Required for shadow-rs to work
 
-#[instrument]
 fn main() -> eyre::Result<()> {
     //Init the important stuff
     init_eyre()?;
     init_tracing()?;
 
-    let _span = info_span!("test_span").entered();
-    let _span = info_span!("child_span").entered();
+    let _span = info_span!("empty_span").entered();
+    let _span = info_span!("test_span", test_prop = "HOLA").entered();
+    let _span = info_span!("child_span", i_am_a_child = "true").entered();
     event!(Level::INFO, "something happened");
+    event!(Level::DEBUG, "something happened");
+    event!(Level::ERROR, "something happened");
+    event!(Level::TRACE, "something happened");
+    event!(Level::WARN, "something happened");
     info!("TEST)");
 
     let string = "string";
@@ -31,32 +38,34 @@ fn main() -> eyre::Result<()> {
     return Err(eyre!("Test lol: {} {asd} {string}", 1, asd = 69420));
 }
 
-/// Tracing filter.
 ///
-/// Can be any of "error", "warn", "info", "debug", or
-/// "trace". Supports more granular filtering, as well; see documentation for
-/// [`tracing_subscriber::EnvFilter`][EnvFilter].
-///
-/// [EnvFilter]: https://docs.rs/tracing-subscriber/latest/tracing_subscriber/struct.EnvFilter.html
-fn init_tracing() -> eyre::Result<()> {
+fn init_tracing() -> eyre::Result<(), TryInitError> {
     use tracing_error::*;
-    use tracing_subscriber::layer::SubscriberExt;
-    use tracing_subscriber::*;
+    use tracing_subscriber::{fmt, layer::SubscriberExt, prelude::*, EnvFilter};
 
-    let standard_layer = fmt::layer()
-        .event_format(format().compact())
+    let standard_format = format()
+        // .compact()
         .with_ansi(true)
         .with_thread_ids(true)
         .with_thread_names(true)
-        .with_target(true)
+        .with_target(false)
+        .with_level(true)
+        .with_timer(time::time())
+        .with_source_location(false)
+        .with_level(true);
+
+    let standard_layer = fmt::layer()
         .with_span_events(FmtSpan::ACTIVE)
         .log_internal_errors(true)
-        .with_file(true)
-        .with_level(true)
-        .with_line_number(true)
-        // .with_timer(time())
+        .event_format(standard_format)
         .with_writer(io::stdout)
+        .with_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::TRACE.into())
+                .from_env_lossy()
+        )
         // .with_test_writer()
+        // .with_timer(time())
         ;
 
     let error_layer = ErrorLayer::default();
@@ -64,13 +73,9 @@ fn init_tracing() -> eyre::Result<()> {
     tracing_subscriber::registry()
         .with(standard_layer)
         .with(error_layer)
-        .init();
-
-    Ok(())
+        .try_init()
 }
 
 fn init_eyre() -> eyre::Result<()> {
-    color_eyre::install()?;
-
-    Ok(())
+    color_eyre::install()
 }
