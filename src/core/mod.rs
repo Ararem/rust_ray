e7f4001dc23ea0fe7dd475c69281ba3e81bb99a5
ararem <48875125+Ararem@users.noreply.github.com>
2022-11-13 16:24:17 +1000

    Trying to reorganise stuff

--- src/core/mod.rs
+++
@@ -1,8 +0,0 @@
-//! Core module that contains important stuff to make the app work at a low level
-//Include the source files
-mod program;
-mod ui;
-
-//Export them publicly for use elsewhere
-pub use program::run_program;
-pub use ui::{init,UiConfig,UiSystem};
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,19 @@
 
 //! A little test raytracer project
 
-mod core;
+mod core::clipboard_integration;
 
 use std::io;
 use std::process::Termination;
 
 use color_eyre::eyre;
 use color_eyre::eyre::WrapErr;
+use glium::{Display, glutin};
+use glium::glutin::event_loop::EventLoop;
+use glium::glutin::window::WindowBuilder;
+use imgui::{Context, FontConfig, FontSource};
+use imgui_glium_renderer::Renderer;
+use imgui_winit_support::{HiDpiMode, WinitPlatform};
 use pretty_assertions::{self, assert_eq, assert_ne, assert_str_eq};
 use shadow_rs::shadow;
 use tracing::metadata::LevelFilter;
@@ -17,8 +23,37 @@ use tracing_subscriber::{
     fmt::{format::*, time},
     util::TryInitError,
 };
+use core::clipboard_integration;
 
 shadow!(build); //Required for shadow-rs to work
+
+///Struct that encapsulates the UI system components
+pub struct UiSystem {
+    pub display: Display,
+    pub event_loop: EventLoop<()>,
+    pub imgui_context: Context,
+    pub platform: WinitPlatform,
+    pub renderer: Renderer,
+}
+
+#[derive(Debug)]
+pub struct UiConfig {
+    pub vsync: bool,
+    pub hardware_acceleration: Option<bool>,
+    /// Optional multisampling
+    pub multisampling: u16,
+}
+
+impl UiConfig {
+    pub fn new() -> UiConfig {
+        UiConfig {
+            hardware_acceleration: None,
+            vsync: true,
+            multisampling: 1,
+        }
+    }
+}
+
 
 /// Main entrypoint for the program
 ///
@@ -32,7 +67,9 @@ fn main() -> eyre::Result<()> {
     init_tracing()?;
     debug!("[tracing] and [eyre] initialised");
 
-    debug!("Skipping CLI args");
+    debug!("Skipping CLI and Env args");
+
+    init_imgui("Test Title for the <APP>", UiConfig{multisampling: 1, vsync: true, hardware_acceleration: Some(false)})?;
 
     info!("Running program");
     core::run_program()?;
@@ -41,7 +78,7 @@ fn main() -> eyre::Result<()> {
     info!("goodbye");
     return Ok(());
 }
-///
+
 fn init_tracing() -> eyre::Result<()> {
     use tracing_error::*;
     use tracing_subscriber::{fmt, layer::SubscriberExt, prelude::*, EnvFilter};
@@ -82,4 +119,114 @@ fn init_tracing() -> eyre::Result<()> {
 
 fn init_eyre() -> eyre::Result<()> {
     color_eyre::install()
+}
+
+///Initialises the UI system and returns it
+///
+/// * `title` - Title of the created window
+/// * `config` - Struct that modifies how the ui system is initialised
+#[instrument]
+pub fn init_imgui(title: &str, config: UiConfig) -> eyre::Result<UiSystem> {
+    let display;
+    let mut imgui;
+    let event_loop;
+
+    {
+        let log_span = debug_span!("creating basic objects").entered();
+        //TODO: More config options
+        trace!("cloning title");
+        let title = title.to_owned();
+        trace!("creating event loop");
+        event_loop = EventLoop::new();
+        trace!("creating glutin context builder");
+        let glutin_context_builder = glutin::ContextBuilder::new() //TODO: Configure
+            .with_vsync(config.vsync)
+            .with_hardware_acceleration(config.hardware_acceleration)
+            .with_multisampling(config.multisampling);
+        trace!("creating window builder");
+        let window_builder = WindowBuilder::new().with_title(title); //TODO: Configure
+    trace!("creating display");
+        display = Display::new(window_builder, glutin_context_builder, &event_loop)
+            .wrap_err("could not initialise display")?;
+        trace!("Creating [imgui] context");
+        imgui = Context::create();
+    }
+
+    {
+        let log_span = debug_span!("trying to enable clipboard support").entered();
+        match clipboard::init() {
+            Ok(clipboard_backend) => {
+                trace!("have clipboard support");
+                imgui.set_clipboard_backend(clipboard_backend);
+            }
+            Err(error) => {
+                warn!("could not initialise clipboard: {error}")
+            }
+        }
+    }
+
+    let mut platform;
+    {
+        let log_span = debug_span!("initialising winit platform").entered();
+        platform = WinitPlatform::init(&mut imgui);
+        let gl_window = display.gl_window();
+        let window = gl_window.window();
+        //TODO: High DPI setting
+        platform.attach_window(imgui.io_mut(), window, HiDpiMode::Default);
+    }
+
+    //TODO: Proper resource manager
+    {
+        let log_span = debug_span!("adding fonts").entered();
+
+        // Fixed font size. Note imgui_winit_support uses "logical
+        // pixels", which are physical pixels scaled by the devices
+        // scaling factor. Meaning, 13.0 pixels should look the same size
+        // on two different screens, and thus we do not need to scale this
+        // value (as the scaling is handled by winit)
+        let font_size = 13.0;
+        let font_config = FontConfig {
+            //TODO: Configure
+            // Oversampling font helps improve text rendering at
+            // expense of larger font atlas texture.
+            oversample_h: 4,
+            oversample_v: 4,
+            size_pixels: font_size,
+            // As imgui-glium-renderer isn't gamma-correct with
+            // it's font rendering, we apply an arbitrary
+            // multiplier to make the font a bit "heavier". With
+            // default imgui-glow-renderer this is unnecessary.
+            rasterizer_multiply: 1.5,
+            //Sets everything to default
+            //Except the stuff we overrode before
+            //SO COOOL!!
+            ..FontConfig::default()
+        };
+
+        let fallback_font = FontSource::DefaultFontData {
+            config: Some(font_config.clone()),
+        };
+        let standard_font = FontSource::TtfData {
+            config: Some(font_config),
+            size_pixels: font_size,
+            data: include_bytes!(
+                "../../resources/fonts/JetBrainsMono-2.242/fonts/ttf/JetBrainsMono-Medium.ttf"
+            ),
+        };
+
+        trace!("standard font: {standard_font:?}, fallback: {fallback_font:?}");
+        let fonts = &[standard_font, fallback_font];
+        imgui.fonts().add_font(fonts);
+    }
+
+    debug!("creating renderer");
+    let renderer = Renderer::init(&mut imgui, &display).wrap_err("failed to create renderer")?;
+
+    Ok(UiSystem {
+        event_loop,
+        display,
+        imgui_context: imgui,
+        platform,
+        renderer,
+    })
 }
