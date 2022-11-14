bd7b74be53d226cfe2176997b069545f7a07b630
ararem <48875125+Ararem@users.noreply.github.com>
2022-11-14 16:30:29 +1000

    Figured out why it wouldn't compile
    https://stackoverflow.com/a/74427311

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ mod program;
 use crate::boilerplate::error_handling::init_eyre;
 use crate::boilerplate::logging::init_tracing;
 use crate::boilerplate::ui_system::{init_imgui, UiConfig};
-use color_eyre::eyre;
+use color_eyre::eyre::eyre;
+use color_eyre::{eyre};
 use glium::glutin::platform::run_return::EventLoopExtRunReturn;
-use glium::{glutin, Surface};
-use pretty_assertions::{self};
 use shadow_rs::shadow;
+use std::process::ExitCode;
 use std::time::Instant;
+use glium::glutin;
+use glium::glutin::event_loop::{ControlFlow, EventLoopWindowTarget};
 use tracing::*;
 
 shadow!(build); //Required for shadow-rs to work
@@ -25,7 +27,7 @@ shadow!(build); //Required for shadow-rs to work
 /// * Initialises `tracing` (for logging)
 /// * Processes command-line arguments
 /// * Runs the program for real
-fn main() -> eyre::Result<()> {
+fn main() -> eyre::Result<ExitCode> {
     init_eyre()?;
     init_tracing()?;
     debug!("[tracing] and [eyre] initialised");
@@ -57,66 +59,21 @@ fn main() -> eyre::Result<()> {
 
     let exit_code:i32 = ui_system
         .event_loop
-        .run_return(|event, _window_target, control_flow| {
-            // trace!("[ui] event: {event:?}");
-            match event {
-                glutin::event::Event::NewEvents(_) => {
-                    ui_system
-                        .imgui_context
-                        .io_mut()
-                        .update_delta_time(last_frame.elapsed());
-                    last_frame = Instant::now();
-                }
-
-                glutin::event::Event::MainEventsCleared => {
-                    let gl_window = ui_system.display.gl_window();
-                    ui_system
-                        .platform
-                        .prepare_frame(ui_system.imgui_context.io_mut(), gl_window.window())
-                        .expect("Failed to prepare frame");
-                    gl_window.window().request_redraw();
-                }
-
-                glutin::event::Event::RedrawRequested(_) => {
-                    let ui = ui_system.imgui_context.frame();
-
-                    //This is where we have to actually do the rendering
-                    // imgui_trace_span.exit();
-                    program.tick(&ui);
-                    // imgui_trace_span.enter();
-
-                    let gl_window = ui_system.display.gl_window();
-                    let mut target = ui_system.display.draw();
-                    target.clear_color_srgb(0.0, 0.0, 0.0, 0.0); //Clear
-                    ui_system.platform.prepare_render(&ui, gl_window.window());
-                    let draw_data = ui.render();
-                    ui_system
-                        .renderer
-                        .render(&mut target, draw_data)
-                        .expect("UI rendering failed");
-                    target.finish().expect("Failed to swap buffers");
-                }
-
-                glutin::event::Event::WindowEvent {
-                    event: glutin::event::WindowEvent::CloseRequested,
-                    ..
-                } => {
-                    *control_flow = glutin::event_loop::ControlFlow::Exit;
-                }
-
-                event => {
-                    let gl_window = ui_system.display.gl_window();
-                    ui_system.platform.handle_event(
-                        ui_system.imgui_context.io_mut(),
-                        gl_window.window(),
-                        &event,
-                    );
-                }
-            }
-        });
+        .run_return(event_loop_run);
 
     drop(_run_span_entered);
 
     info!("exit_code: {exit_code}");
-    exit_code
+
+    return match exit_code {
+        0 => Ok(ExitCode::SUCCESS),
+        code => Err(eyre!(
+            "run loop returned non-zero exit code {:?}",
+            code
+        )),
+    };
+}
+
+fn event_loop_run(event: glium::glutin::event::Event<()>, window_target: &EventLoopWindowTarget<()>, control_flow: &mut ControlFlow){
+
 }
