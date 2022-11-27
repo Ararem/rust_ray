ae23dba272c0e7edfe6195b55628ec40eb48bc18
ararem <48875125+Ararem@users.noreply.github.com>
2022-11-27 22:08:48 +0100

    Trying to get resources to copy to output directory

--- build.rs
+++ build.rs
@@ -1,3 +1,29 @@
-fn main() -> shadow_rs::SdResult<()> {
-    shadow_rs::new()
+use std::env;
+use std::error::Error;
+use fs_extra::copy_items;
+use fs_extra::dir::CopyOptions;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    //Copy /resources to our output directory
+    {
+        const RESOURCES_FOLDER_LOCATION: &str = "resources";
+
+        // Re-runs script if any files in res are changed
+        println!("cargo:rerun-if-changed={RESOURCES_FOLDER_LOCATION}/*");
+
+        let mut options = CopyOptions::new();
+        // Overwrite existing files with same name
+        options.overwrite = true;
+
+        let mut from_path = Vec::new();
+        from_path.push(RESOURCES_FOLDER_LOCATION);
+
+        let out_path = env::var_os("CARGO_BUILD_TARGET_DIR ").unwrap();
+
+        // copy_items(&from_path, &out_path, &options)?;
+    }
+
+    shadow_rs::new()?;
+
+    Ok(())
 }
--- src/config/ui_config.rs
+++ src/config/ui_config.rs
@@ -7,23 +7,6 @@ pub const UI_CONFIG: UiConfig = UiConfig {
     hardware_acceleration: Some(true),
 };
 
-pub const FONT_SIZES: [f32; 8] = [10f32, 12f32, 16f32, 24f32, 32f32, 40f32, 48f32, 64f32];
-pub const DEFAULT_FONT_SIZE_INDEX: usize = 3;
-
-pub const BUILTIN_FONTS: &[Font] = &[
-    Font{
-        name: "Consolas v5.53"
-    }
-];
-// Indices corresponding to the default font, in this case JB Mono @ Regular
-pub const DEFAULT_FONT_INDEX: usize = 0;
-pub const DEFAULT_FONT_VARIANT_INDEX: usize = 3;
-
-// Fixed font size. Note imgui_winit_support uses "logical
-// pixels", which are physical pixels scaled by the devices
-// scaling factor. Meaning, 15.0 pixels should look the same size
-// on two different screens, and thus we do not need to scale this
-// value (as the scaling is handled by winit)
 pub fn base_font_config() -> FontConfig {
     FontConfig {
         //TODO: Configure
