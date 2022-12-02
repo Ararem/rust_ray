6267d51740a933ecfa9770613f5bfb3de2736d5b
ararem <48875125+Ararem@users.noreply.github.com>
2022-12-02 21:56:49 +0100

    Rename and reorganise fonts

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::error::Error;
 
 use fs_extra::copy_items;
-use fs_extra::dir::CopyOptions;
+use fs_extra::dir::{CopyOptions, remove};
 
 // macro_rules! p {
 //     ($($tokens: tt)*) => {
@@ -13,7 +13,9 @@ use fs_extra::dir::CopyOptions;
 fn main() -> Result<(), Box<dyn Error>> {
     //Copy /resources to our output directory
     {
-        const RESOURCES_FOLDER_LOCATION: &str = "src/resources/app_resources";
+        // TODO: Refactor this resources folder code so it's less hacky, and sse the path API
+        const RESOURCES_FOLDER_NAME: &str = "app_resources";
+        const RESOURCES_FOLDER_LOCATION: &str = "src/resources";
 
         // Re-runs script if any files in res are changed
         println!("cargo:rerun-if-changed={RESOURCES_FOLDER_LOCATION}/*");
@@ -24,13 +26,13 @@ fn main() -> Result<(), Box<dyn Error>> {
             o
         };
 
-        // TODO: Use the path API for this???
-        let source_path = format!("{}/{}", env::var("CARGO_MANIFEST_DIR")?, RESOURCES_FOLDER_LOCATION);
+        let source_path = format!("{}/{}/{}", env::var("CARGO_MANIFEST_DIR")?, RESOURCES_FOLDER_LOCATION, RESOURCES_FOLDER_NAME);
         // Have to jump up a three levels because cargo adds some extra directories: "\rust_ray\target\debug\build\rust_ray-e17a28a2c53dbfbd\out"
         let dest_path = format!("{}/../../../", env::var("OUT_DIR")?);
 
         // p!("src:{source_path}");
         // p!("dest:{dest_path}");
+        remove(format!("{}/{}", dest_path, RESOURCES_FOLDER_NAME))?; //Get rid of the old resources folder
         copy_items(&vec![source_path], &dest_path, &options)?;
     }
 
